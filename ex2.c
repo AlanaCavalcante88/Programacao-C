f46e948fdf988359db4920edf2de86919e1f6494
Alana Cavalcante <alanacristinaf@gmail.com>
2025-03-18 19:03:18 -0300

    alteracoes

--- ex2.c
+++ ex2.c
@@ -15,19 +15,33 @@ int main() {
 printf("****** CALCULADORA ******");
 
 float num1, num2;
-printf("\nDigite um valor:");
-scanf("%f", &num1);
-printf("Digite outro valor:");
-scanf("%f", &num2);
 
 char operacao;
-printf("Qual operação você deseja realizar?(+,-,*,/):");
+printf("\nQual operação você deseja realizar?(+,-,*,/):");
 scanf(" %c", &operacao); //o espaço antes de %c é para ele entender que tem um espaço quando vc digita num1 e num2
 
 
-if (operacao == '+') {
+switch (operacao) { 
+    case '+':
+    printf("\nDigite dois valores:");
+    scanf("%f%f", &num1, &num2);
     printf("\nResultado: %.2f + %.2f = %.2f\n", num1, num2, num1 + num2);
-} else if (operacao == '-') {
+    break;
+
+    case '-': 
+    printf("\nDigite dois valores:");
+    scanf("%f%f", &num1, &num2);
     printf("\nResultado: %.2f - %.2f = %.2f\n", num1, num2, num1 - num2);
-}
+
+    case '*': 
+    printf("\nDigite dois valores:");
+    scanf("%f%f", &num1, &num2);
+    printf("\nResultado: %.2f * %.2f = %.2f\n", num1, num2, num1 * num2);
+
+    case '/': 
+    printf("\nDigite dois valores:");
+    scanf("%f%f", &num1, &num2);
+    printf("\nResultado: %.2f / %.2f = %.2f\n", num1, num2, num1 / num2);
+
+  }
 }
